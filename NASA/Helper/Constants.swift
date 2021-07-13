08835f71dd89bb1cce27b189d4b156989060bb20
Haresh Ghatala <wb-hareshbha878354@antgroup.com>
2021-07-13 21:00:35 +0530

    NASA API - Added helper classes

---
+++ NASA/Helper/Constants.swift
@@ -0,0 +1,23 @@
+//
+//  Constants.swift
+//  NASA
+//
+//  Created by Haresh Ghatala on 13/07/21.
+//  Copyright © 2021 Haresh Ghatala. All rights reserved.
+//
+
+public let imgNamePlaceholder = "placeholder"
+
+public let showDetailSegueId = "showDetailSegue"
+
+public let mediaTypeText = "image"
+public let notFoundText = "Not Found"
+public let listDateForamt = "dd MMM, yyyy"
+public let listItemCellId = "ListItemCell"
+
+public let retryText = "Retry"
+
+public let errorTitle = "That didn’t work!"
+public let errorMsgInvalidEndpoint = "Invalid URL or Endpoint. Please retry."
+public let errorMsgInvalidResponse = "Invalid response or Data issue. Please retry."
+public let errorMsgUnknown = "Unknown Error. Please retry."
